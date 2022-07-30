97dc14bb220b327d1bab728d2ac16dd4acc0c2da
shaunsingh <shaunsingh0207@gmail.com>
2022-07-30 16:14:35 -0400

    (script) fix install script

--- install.sh
+++ install.sh
@@ -2,31 +2,16 @@
 
 set -e
 
-# The root of the project.
-PRJ_ROOT="${PRJ_ROOT:-$(git rev-parse --show-toplevel)}"
+cargo build --release 
 
-cargo_build() {
-  if ! command -v cargo &>/dev/null; then
-    echo "Couldn't find cargo in \$PATH, make sure the Rust toolchain is installed"
-    return 1
-  fi
-  cargo build --release 
-  return 0
-}
+# Place the compiled library where Neovim can find it.
+mkdir -p lua
 
-copy_stuff() {
-  # extension is `.so` on linux, `.dylib` on macOS and `.dll` on Windows
-  library_extension=$(\
-    [ -f $PRJ_ROOT/target/release/liboxocarbon.so ] \
-      && echo so \
-      || echo dylib \
-  )
+if [ "$(uname)" == "Darwin" ]; then
+    mv target/release/liboxocarbon.dylib lua/oxocarbon.so
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    mv target/release/liboxocarbon.so lua/oxocarbon.so
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
+    mv target/release/oxocarbon.dll lua/oxocarbon.dll
+fi
 
-  # Place the compiled library where Neovim can find it.
-  mkdir -p $PRJ_ROOT/lua
-  cp \
-    "$PRJ_ROOT/target/release/liboxocarbon.$library_extension" \
-    $PRJ_ROOT/lua/oxocarbon.so
-}
-
-cargo_build && copy_stuff
